7cfc2196a41820a29d6207cde2aa4d5601fa728f
Eric Dean Watt <eric.d.watt@gmail.com>
2016-08-19 16:29:44 -0400

    Started script to query ER data to include as data in the package.

---
+++ data-raw/er_example_data.R
@@ -0,0 +1,25 @@
+# Script to generate sample data to include with eapath
+# this data represents what would be returned from the tcpl database
+# to be used as input for the ER Pathway Model.
+
+library(tcpl)
+library(magrittr)
+tcplConf(db = "prod_external_invitrodb_v2")
+
+# Get aeids corresponding to ER assays
+
+assays <- c("NVS_NR_bER", "NVS_NR_hER", "NVS_NR_mERa", "OT_ER_ERaERa_0480",
+            "OT_ER_ERaERa_1440", "OT_ER_ERaERb_0480", "OT_ER_ERaERb_1440",
+            "OT_ER_ERbERb_0480", "OT_ER_ERbERb_1440", "OT_ERa_EREGFP_0120",
+            "OT_ERa_EREGFP_0480", "ATG_ERa_TRANS_up", "ATG_ERE_CIS_up",
+            "TOX21_ERa_BLA_Agonist_ratio", "TOX21_ERa_LUC_BG1_Agonist",
+            "ACEA_T47D_80hr_Positive", "TOX21_ERa_BLA_Antagonist_ratio",
+            "TOX21_ERa_LUC_BG1_Antagonist")
+
+aeid_table_full <- tcplLoadAeid(add.fld = c("asid", "asnm"))
+aeids <- aeid_table_full[aenm %in% assays, aeid]
+
+dat_L5 <- tcplLoadData(lvl = 5L, fld = "aeid", val = aeids, type = "mc") %>%
+    tcplPrepOtpt
+
+#devtools::use_data(erl5data, overwrite = TRUE)
