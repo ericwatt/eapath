aea1c1bb37c07e90b48c8dcb0eea7b9a6caa95e3
Eric Dean Watt <eric.d.watt@gmail.com>
2016-08-20 00:40:50 -0400

    Documentation of included ER dataset, added data-raw script and added data-raw to .Rbuildignore

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' ER ToxCast Data
+#'
+#' A dataset containing ToxCast model parameters, hit calls, assays, and
+#' chemical info to generate ER Model AUC values. Data is pulled from the
+#' October 2015 public release (prod_external_invitrodb_v2), and subset down to
+#' the minimum needed to run the model.
+#'
+#' @format A data.table with 60330 rows and 9 columns
+#'
+"er_L5_prod_ext_v2"
